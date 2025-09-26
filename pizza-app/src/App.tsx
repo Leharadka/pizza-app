ebdf687e6b30ae654b19ecab650c51fa006427c1
Aleksei <alex.porfirov@gmail.com>
2025-09-26 11:59:12 +0300

    Добавил роутинг

--- pizza-app/src/App.tsx
+++ pizza-app/src/App.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import Button from "./components/Button/Button";
-import Input from './components/Input/Input';
+import Input from "./components/Input/Input";
+import { Route, Routes } from "react-router-dom";
+import { Menu } from "./pages/Menu/Menu";
+import { Cart } from "./pages/Cart/Cart";
+import { Error } from './pages/Error/Error';
 
 function App() {
   const [counter, setCounter] = useState<number>(0);
@@ -12,8 +16,19 @@ function App() {
   return (
     <>
       <Button onClick={addCounter}>Кнопка</Button>
-      <Button appearence='big' onClick={addCounter}>Кнопка</Button>
-      <Input placeholder='email'/>
+      <Button appearence="big" onClick={addCounter}>
+        Кнопка
+      </Button>
+      <Input placeholder="email" />
+      <div>
+        <a href="/">Меню</a>
+        <a href="/cart">Корзина</a>
+      </div>
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </>
   );
 }
