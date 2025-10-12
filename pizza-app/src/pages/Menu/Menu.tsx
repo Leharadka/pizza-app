2d763a48cdbc3003e0dafc7f87cc24e7e55a7518
Aleksei <alex.porfirov@gmail.com>
2025-10-12 15:47:51 +0300

    Сверстал карточку товара

--- pizza-app/src/pages/Menu/Menu.tsx
+++ pizza-app/src/pages/Menu/Menu.tsx
@@ -1,4 +1,5 @@
 import Headling from "../../components/Headling/Headling";
+import ProductCard from "../../components/ProductCart/ProductCard";
 import Search from "../../components/Search/Search";
 import styles from "./Menu.module.css";
 
@@ -7,7 +8,17 @@ export function Menu() {
     <>
       <div className={styles["head"]}>
         <Headling>Меню</Headling>
-        <Search placeholder='Введите блюдо или состав' />
+        <Search placeholder="Введите блюдо или состав" />
+      </div>
+      <div>
+        <ProductCard
+          id={1}
+          title={"Наслождение"}
+          description={"Салями, руккола, помидоры, оливки"}
+          price={300}
+          rating={4.5}
+          image={"/product-demo.png"}
+        />
       </div>
     </>
   );
