67975d817954cfa76f48bdeacf39624feb393511
Aleksei <alex.porfirov@gmail.com>
2025-10-20 15:11:02 +0300

    add loader

--- pizza-app/src/main.tsx
+++ pizza-app/src/main.tsx
@@ -5,8 +5,10 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Menu } from "./pages/Menu/Menu.tsx";
 import { Cart } from "./pages/Cart/Cart.tsx";
 import { Error } from "./pages/Error/Error.tsx";
-import { Layout } from './layout/Menu/Layout.tsx';
-import { Product } from './pages/Product/Product.tsx';
+import { Layout } from "./layout/Menu/Layout.tsx";
+import { Product } from "./pages/Product/Product.tsx";
+import axios from "axios";
+import { PREFIX } from "./helpers/API.ts";
 
 const router = createBrowserRouter([
   {
@@ -28,9 +30,18 @@ const router = createBrowserRouter([
     element: <Error />,
   },
   {
-    path: '/product/:id',
-    element: <Product/>
-  }
+    path: "/product/:id",
+    element: <Product />,
+    loader: async ({ params }) => {
+      await new Promise<void>((resolve) => {
+        setTimeout(() => {
+          resolve;
+        }, 2000);
+      });
+      const { data } = await axios.get(`${PREFIX}/products/${params.id}`);
+      return data;
+    },
+  },
 ]);
 
 createRoot(document.getElementById("root")!).render(
