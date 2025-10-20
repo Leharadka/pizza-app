417ba8770165aa5130c8f3fdc5dc3edffa5a1765
Aleksei <alex.porfirov@gmail.com>
2025-10-20 14:58:37 +0300

    обработка загрузки + обработка ошибки

--- pizza-app/src/pages/Menu/Menu.tsx
+++ pizza-app/src/pages/Menu/Menu.tsx
@@ -1,21 +1,34 @@
 import { useEffect, useState } from "react";
 import Headling from "../../components/Headling/Headling";
-import ProductCard from "../../components/ProductCart/ProductCard";
 import Search from "../../components/Search/Search";
 import { PREFIX } from "../../helpers/API";
 import type { Product } from "../../interfaces/product.interface";
 import styles from "./Menu.module.css";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { MenuList } from "./MenuList/MenuList";
 
 export function Menu() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | undefined>();
 
   const getmenu = async () => {
     try {
+      setIsLoading(true);
+      await new Promise<void>((resolve) => {
+        setTimeout(() => {
+          resolve;
+        }, 2000);
+      });
       const { data } = await axios.get<Product[]>(`${PREFIX}/products`);
       setProducts(data);
+      setIsLoading(false);
     } catch (e) {
       console.error(e);
+      if (e instanceof AxiosError) {
+        setError(e.message);
+      }
+      setIsLoading(false);
       return;
     }
   };
@@ -31,17 +44,10 @@ export function Menu() {
         <Search placeholder="Введите блюдо или состав" />
       </div>
       <div>
-        {products.map((p) => (
-          <ProductCard
-            key={p.id}
-            id={p.id}
-            name={p.name}
-            description={p.ingredients.join(", ")}
-            price={p.price}
-            rating={p.rating}
-            image={p.image}
-          />
-        ))}
+        {error && <>{error}</>}
+        {!isLoading && <MenuList products={products} />}
+
+        {isLoading && <>Загружаем продукты...</>}
       </div>
     </>
   );
